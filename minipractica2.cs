9dce9df4df83dd0fc9f4c6dc3e509064fc917964
MashkarinaKate <63970638+MashkarinaKate@users.noreply.github.com>
2020-04-24 19:08:38 +0300

    Update minipractica2.cs

--- minipractica2.cs
+++ minipractica2.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD:main.cs
-using System;  
-  public class FibonacciExample  
-   {  
-     public static void Main(string[] args)  
-      {  
-         int n1=0,n2=1,n3,i,number;    
-         Console.Write("Enter the number of elements: ")
-         number = int.Parse(Console.ReadLine());  
-         Console.Write(n1+" "+n2+" "); 
-         for(i=2;i<number;++i)  
-         {
-          n3=n1+n2;    
-          Console.Write(n3+" ")    
-          n1=n2;    
-          n2=n3;    
-         }    
-      }  
-=======
 using System;
 
 class MainClass {
@@ -29,4 +10,3 @@ class MainClass {
        ////////////////////
   }
 }
->>>>>>> abd31d2ddf53354d906bdbbc946b1144588c70d4:minipractica2.cs
